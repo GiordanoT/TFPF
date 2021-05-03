cebb60b7be567136b9a281dd96d640f54b07fa73
Gianlucadimarzio <gianluca.dimarzio1@student.univaq.it>
2021-05-03 11:10:51 +0200

    test

---
+++ test/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+require_once './vendor/autoload.php';
+
+
+class ExampleTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertEquals(0, 1);
+    }
+}
