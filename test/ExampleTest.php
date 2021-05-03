1a93af71439b4d83002b7f4ee34f616352ecb45c
Gianlucadimarzio <gianluca.dimarzio1@student.univaq.it>
2021-05-03 11:39:19 +0200

    test

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -7,18 +7,16 @@ use PHPUnit\Framework\TestCase;
 require_once './vendor/autoload.php';
 //require_once './Funzione.php';
 
+ function somma($n){
+    return $n+1;
+} 
 
 class ExampleTest extends TestCase
 {
-    public function somma($n){
-        return $n+1;
-    }
 
     public function test_example()
     {
-        $n = (Object) 0;
         //$this->assertEquals(2, 2);
-        $n-> somma(3);
-        $this->assertEquals(4, $n);
+        $this->assertEquals(4, somma(3));
     }
 }
