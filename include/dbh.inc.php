655bf4ac3c532d4b312372b034eab3e4bc7d9779
GiordanoT <giordanotinella@gmail.com>
2021-05-11 11:10:46 +0200

    link DB

---
+++ include/dbh.inc.php
@@ -0,0 +1,50 @@
+<?php
+  $servername = "localhost";
+  $dBUsername = "root";
+  $dBPassword = "";
+  $dBName = "globex_corporation";
+
+  $conn = mysqli_connect( $servername, $dBUsername, $dBPassword, $dBName );
+  if( !$conn ) {
+    header( "Location: ../error.php" );
+	  //require 'component/error.component.php';
+  	exit();
+  }
+  function getData( $sql ){
+    $connection = mysqli_connect( $GLOBALS['servername'], $GLOBALS['dBUsername'], $GLOBALS['dBPassword'], $GLOBALS['dBName'] );
+    if( !$connection ){
+      header("Location: ../error.php");
+	    //require 'component/error.component.php';
+      exit();
+    }
+    $stmt = mysqli_stmt_init( $connection );
+    if( !mysqli_stmt_prepare( $stmt, $sql ) ) {
+      header( "Location: ../error.php" );
+	    //require 'component/error.component.php';
+      exit();
+    }
+    $queryResult = array();
+    mysqli_stmt_execute( $stmt);
+    $result = mysqli_stmt_get_result( $stmt );
+    while( $row = mysqli_fetch_assoc( $result ) ){
+      array_push( $queryResult, $row );
+    }
+    mysqli_close( $connection );
+    return $queryResult;
+  }
+  function setData( $sql ){
+    $connection = mysqli_connect( $GLOBALS['servername'], $GLOBALS['dBUsername'], $GLOBALS['dBPassword'], $GLOBALS['dBName'] );
+    if( !$connection ){
+      header( "Location: ../error.php" );
+	    //require 'component/error.component.php';
+      exit();
+    }
+    $stmt = mysqli_stmt_init( $connection );
+    if ( !mysqli_stmt_prepare( $stmt, $sql ) ) {
+      header( "Location: ../error.php" );
+	    //require 'component/error.component.php';
+      exit();
+    }
+    mysqli_stmt_execute( $stmt );
+  }
+?>
