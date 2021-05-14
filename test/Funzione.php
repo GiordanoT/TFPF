fd5ee68e56ff46dbfe134649e9636c3a8e36569c
GiordanoT <giordanotinella@gmail.com>
2021-05-14 13:36:06 +0200

    Final test

--- test/Funzione.php
+++ test/Funzione.php
@@ -1,13 +1,9 @@
 <?php
-//require_once( "../include/dbh.inc.php" );
+
 function ricercaEventi( $s ){
 	$query = "SELECT * FROM evento WHERE nome LIKE '%{$s}%'";
 	$resultEvento = getData( $query );
 	return $resultEvento;
-}/*
-$item = ricercaEventi( "int" );
-echo $item.get_class();
-$flag = assertEquals( Array(), $item.get_class() );
-echo $flag;*/
+}
 
 ?>
