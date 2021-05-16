8928cd4baa22aad85e04642fd3e8a9e8e3167be5
federiico <federicocantoro99@gmail.com>
2021-05-16 15:08:59 +0200

    fix login

--- include/login.inc.php
+++ include/login.inc.php
@@ -4,29 +4,31 @@
     
     $email = $_POST['email'];
     $password = $_POST['password'];
+
+    $email = addslashes( $email );
+    $email = strip_tags( $email );
+
+    $password = addslashes( $password );
+    $password = strip_tags( $password );
    
-    if($email == '' || $password == ''){
-        header("Location: ../login.php?error=empty_fields");
-    }
+    $query = "SELECT id,nome,cognome,email,password,ruolo FROM utente WHERE email ='".$email."' AND password ='".$password."' ";
+
+    $resultUtenti = getData($query);
+
+    if(count($resultUtenti) != 1)
+        header("Location: ../login.php?error=bad_login");
     else{
-        $query = "SELECT id,nome,cognome,email,password,ruolo FROM utente WHERE email ='".$email."' AND password ='".$password."' ";
-
-        $resultUtenti = getData($query);
-
-        if(count($resultUtenti) != 1)
-            header("Location: ../login.php?error=bad_login");
-        else{
-            session_start();
-            foreach($resultUtenti as $rowUtente ){   
-                $_SESSION['id'] = $rowUtente['id'];
-                $_SESSION['nome'] = $rowUtente['nome'];
-                $_SESSION['cognome'] = $rowUtente['cognome'];
-                $_SESSION['mail'] = $rowUtente['email'];
-                $_SESSION['password'] = $password;
-                $_SESSION['ruolo'] = $rowUtente['ruolo'];
-            }
-            header("Location: ../home.php");
+        session_start();
+        foreach($resultUtenti as $rowUtente ){   
+            $_SESSION['id'] = $rowUtente['id'];
+            $_SESSION['nome'] = $rowUtente['nome'];
+            $_SESSION['cognome'] = $rowUtente['cognome'];
+            $_SESSION['mail'] = $rowUtente['email'];
+            $_SESSION['password'] = $password;
+            $_SESSION['ruolo'] = $rowUtente['ruolo'];
         }
+        header("Location: ../home.php");
     }
+    
 
 ?>
