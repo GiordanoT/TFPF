ebcb98e2cc0b195e13bcfd8e121b63928b28b7ab
DavPalo <davide.palombaro@yahoo.com>
2021-05-17 10:17:47 +0200

    Test Finale Davide

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -11,14 +11,14 @@ require_once 'include/dbh.inc.php';
 class ExampleTest extends TestCase
 {
 
-    public function test_example()
-    {
+    public function test_example(){
+
         //$this->assertEquals(2, 2);
         //registrazione($email,$password,$nome,$cognome)
         //modifica_preferiti($cat,$mail,$del)
 
         //$this->assertEquals( 2, modifica_preferiti('db','db','db') );
-        $this->assertEquals( 1, modifica_preferiti('2','giordano@mail.it','0'));
+        //$this->assertEquals( 1, modifica_preferiti('2','giordano@mail.it','0'));
         $this->assertEquals( 1, modifica_preferiti('2','giordano@mail.it','1'));
         $this->assertEquals( 0, modifica_preferiti('1','margherita@mail.it','1'));
         $this->assertEquals( 0, registrazione('giordano@mail.it','123','prova','prova'));
