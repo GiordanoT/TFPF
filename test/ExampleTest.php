baa9d7e3dd78c17d8231e2336d8590b26732db08
GiordanoT <giordanotinella@gmail.com>
2021-05-17 11:00:51 +0200

    test

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -5,7 +5,7 @@ namespace Test;
 use PHPUnit\Framework\TestCase;
 
 require_once './vendor/autoload.php';
-require_once 'Funzione.php';
+//require_once 'Funzione.php';
 require_once '../include/dbh.inc.php';
 
 class ExampleTest extends TestCase{
