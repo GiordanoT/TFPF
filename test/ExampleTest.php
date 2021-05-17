1cf734776da02e7bd91c6a5e33c08da0079c51bd
GiordanoT <giordanotinella@gmail.com>
2021-05-17 10:54:06 +0200

    test

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -8,16 +8,10 @@ require_once './vendor/autoload.php';
 require_once 'Funzione.php';
 require_once '../include/dbh.inc.php';
 
-class ExampleTest extends TestCase
-{
+class ExampleTest extends TestCase{
 
-    public function test_example()
-    {
-        $this->assertEquals(true,login("gianluca@mail.it","123"));
-        $this->assertEquals(false,login("margherita@mail.it","125"));
-        $this->assertEquals(false,login("davide@mail.it","123"));
-        $this->assertEquals(false,login("","125"));
-        $this->assertEquals(false,login("gianluca@mail.it",""));
+    public function test_example(){
+        $this->assertEquals( 1, setData("INSERT INTO categoria (nome,immagine) VALUES ("rugby","")));
     }
 }
 
