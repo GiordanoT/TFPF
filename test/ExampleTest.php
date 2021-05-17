2cc98bf2fcfd034470bc5fc6e6b827d532106290
GiordanoT <giordanotinella@gmail.com>
2021-05-17 09:15:09 +0200

    Test Finale Giordano

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -18,5 +18,9 @@ class ExampleTest extends TestCase
         $this->assertEquals( 0, ricercaEventi("inte") );
         $this->assertEquals( 0, ricercaEventi("int") );
         $this->assertEquals( 0, ricercaEventi("in") );
+        $this->assertEquals( 0, ricercaEventi("i") );
+        $this->assertEquals( 0, ricercaEventi("") );
+        $this->assertEquals( 0, ricercaEventi("mila") );
+        $this->assertEquals( 0, ricercaEventi("juve") );
     }
 }
