47456f27da9d51058f310afd9e7987c0cc15dc6e
GiordanoT <giordanotinella@gmail.com>
2021-05-23 09:56:34 +0200

    fix test

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -50,10 +50,13 @@ class ExampleTest extends TestCase{
       $this->assertEquals( 0, creaEvento("Gran Premio di Monaco","F1 - GP di Monaco",1,2,0,13,150.00,"foto.jpg",0) );
       $this->assertEquals( 0, creaEvento("Gran Premio di Monaco","F1 - GP di Monaco",1,2,12000,13,-150.00,"foto.jpg",0) );
 
-      $this->assertEquals( 1, scegliDate(2,array(0,"2021-05-22","2021-05-23"),array(0,"12:00","15:00"),array(0,"13:00","16:00")) );
+      //$this->assertEquals( 1, scegliDate(2,array(0,"2021-05-22","2021-05-23"),array(0,"12:00","15:00"),array(0,"13:00","16:00")) );
       $this->assertEquals( 0, scegliDate(2,array(0,"2021-05-20","2021-05-23"),array(0,"12:00","15:00"),array(0,"13:00","16:00")) );
       $this->assertEquals( 0, scegliDate(2,array(0,"2021-05-22","2021-05-23"),array(0,"12:00","15:00"),array(0,"10:00","16:00")) );
       $this->assertEquals( 0, scegliDate(2,array(0,"2021-05-22","2021-05-22"),array(0,"12:00","15:00"),array(0,"13:00","16:00")) );
+
+      $query = "DELETE FROM evento WHERE nome='Gran Premio di Monaco'";
+      $this->assertEquals( 1, setData( $query ) );
 
     }
 }
