60d7a91577c0ab28efd605bbbd19aed5f1c9f83e
GiordanoT <giordanotinella@gmail.com>
2021-05-26 15:48:29 +0200

    fix test

--- test/LoginTest.php
+++ test/LoginTest.php
@@ -5,22 +5,11 @@ namespace Test;
 use PHPUnit\Framework\TestCase;
 
 require_once './vendor/autoload.php';
+
 require_once 'include/dbh.inc.php';
-
 require_once 'Login.php';
-require_once 'Signin.php';
-require_once 'CategoriaPreferita.php';
-require_once 'RicercaEventi.php';
-require_once 'RicercaEventiCategoria.php';
-require_once 'EventiCalendarioPref.php';
-require_once 'EventiCalendarioPar.php';
-require_once 'CreaEvento.php';
-require_once 'ScegliDate.php';
-require_once 'Functions/AdminSconto.php';
-require_once 'Functions/UtenteSconto.php';
 
-
-class ExampleTest extends TestCase{
+class LoginTest extends TestCase{
 
     public function login_test(){
       $this->assertEquals( 2, Login( "ciao@ciao.ciao","123" ) );
