835dd6806976556cfa566d69e66a6521b17ee412
federiico <federicocantoro99@gmail.com>
2021-05-28 16:54:02 +0200

    modifica_evento senza notifiche

--- test/ModificaEventoTest.php
+++ test/ModificaEventoTest.php
@@ -9,7 +9,7 @@ require_once 'include/dbh.inc.php';
 
 require_once 'include/functions/modifica_evento.fun.php';
 
-class MoEventoTest extends TestCase{
+class ModificaEventoTest extends TestCase{
 
     public function test_example(){
         
@@ -25,10 +25,10 @@ class MoEventoTest extends TestCase{
       
         $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,array(0,"2021-05-20","2021-05-30"),2,array(0,"2021-05-20","2021-05-31"),array(0,"18:00","9:00"),array(0,"21:00","10:00"),array(0,0,30),120));
         $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,array(0,"2021-05-20","2021-05-31"),3,array(0,"2021-05-20","2021-05-30","2021-06-01"),array(0,"18:00","9:00","10:00"),array(0,"21:00","10:00","11:00"),array(0,0,30,30),120));
-        $this->assertEquals( 0, ModificaEvento(19,$query_evento_1,array(0,"2021-05-20","2021-05-31"),3,array(0,"2021-05-20","2021-05-31","2021-06-01"),array(0,"18:00","9:00","10:00"),array(0,"21:00","10:00","9:00"),array(0,0,30,30),120));
-        $this->assertEquals( 0, ModificaEvento(19,$query_evento_0,array(0,"2021-05-20","2021-05-31"),3,array(0,"2021-05-20","2021-05-31","2021-06-01"),array(0,"18:00","9:00","10:00"),array(0,"21:00","10:00","11:00"),array(0,0,30,30),120));
+        $this->assertEquals( 0, ModificaEvento(19,$query_evento_1,array(0,"2021-05-20","2021-05-30","2021-06-01"),3,array(0,"2021-05-20","2021-05-31","2021-06-01"),array(0,"18:00","9:00","10:00"),array(0,"21:00","10:00","9:00"),array(0,0,30,30),120));
+        $this->assertEquals( 0, ModificaEvento(19,$query_evento_0,array(0,"2021-05-20","2021-05-30","2021-06-01"),3,array(0,"2021-05-20","2021-05-31","2021-06-01"),array(0,"18:00","9:00","10:00"),array(0,"21:00","10:00","11:00"),array(0,0,30,30),120));
         $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,array(0,"2021-05-20","2021-05-31","2021-06-01"),2,array(0,"2021-05-20","2021-05-30"),array(0,"18:00","9:00"),array(0,"21:00","10:00"),array(0,0,30),120));
-        $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,array(0,"2021-05-20","2021-05-31"),1,array(0,"2021-05-20"),array(0,"18:00"),array(0,"21:00"),array(0,0),120));
+        $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,array(0,"2021-05-20","2021-05-30"),1,array(0,"2021-05-20"),array(0,"18:00"),array(0,"21:00"),array(0,0),120));
      
         $query = "DELETE FROM evento WHERE nome='Evento Test'";
         $this->assertEquals( 1, setData( $query ) );
