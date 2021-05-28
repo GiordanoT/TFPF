0fd3a2e2d8b86a91e79841c6c5557d2852500f82
GiordanoT <giordanotinella@gmail.com>
2021-05-28 11:04:10 +0200

    fix test

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
   error_reporting(E_ALL ^ (E_NOTICE | E_WARNING) );
   $db = 1;
@@ -49,55 +48,3 @@
     return 1;
   }
 ?>
-=======
-<?php
-  error_reporting(E_ALL ^ (E_NOTICE | E_WARNING) );
-  $db = 1;
-  if( $db == 1 ){
-    //DB ONLINE
-    $servername = "sql11.freesqldatabase.com";
-    $dBUsername = "sql11414764";
-    $dBPassword = "upJtnWXz89";
-    $dBName = "sql11414764";
-  } else {
-    //DB LOCALE
-    $servername = "localhost";
-    $dBUsername = "root";
-    $dBPassword = "";
-    $dBName = "globex_corporation";
-  }
-
-
-  function getData( $sql ){
-    $connection = mysqli_connect( $GLOBALS['servername'], $GLOBALS['dBUsername'], $GLOBALS['dBPassword'], $GLOBALS['dBName'] );
-    if( !$connection ){
-      return 0;
-    }
-    $stmt = mysqli_stmt_init( $connection );
-    if( !mysqli_stmt_prepare( $stmt, $sql ) ) {
-      return 0;
-    }
-    $queryResult = array();
-    mysqli_stmt_execute( $stmt);
-    $result = mysqli_stmt_get_result( $stmt );
-    while( $row = mysqli_fetch_assoc( $result ) ){
-      array_push( $queryResult, $row );
-    }
-    mysqli_close( $connection );
-    return $queryResult;
-  }
-
-  function setData( $sql ){
-    $connection = mysqli_connect( $GLOBALS['servername'], $GLOBALS['dBUsername'], $GLOBALS['dBPassword'], $GLOBALS['dBName'] );
-    if( !$connection ){
-      return 0;
-    }
-    $stmt = mysqli_stmt_init( $connection );
-    if ( !mysqli_stmt_prepare( $stmt, $sql ) ) {
-      return 0;
-    }
-    mysqli_stmt_execute( $stmt );
-    return 1;
-  }
-?>
->>>>>>> 44538b8605e5d51332437bf48cd2654c97236c46
--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -14,16 +14,6 @@ require_once 'RicercaEventi.php';
 require_once 'RicercaEventiCategoria.php';
 require_once 'EventiCalendarioPref.php';
 require_once 'EventiCalendarioPar.php';
-<<<<<<< HEAD
-require_once 'CreaEvento.php';
-require_once 'ScegliDate.php';
-require_once 'Functions/AdminSconto.php';
-require_once 'Functions/UtenteSconto.php';
-=======
-require_once 'include/functions/crea_evento.fun.php';
-require_once 'include/functions/scegli_date.fun.php';
->>>>>>> 44538b8605e5d51332437bf48cd2654c97236c46
-
 
 class ExampleTest extends TestCase{
 
@@ -32,7 +22,7 @@ class ExampleTest extends TestCase{
       $this->assertEquals( 2, Login( "prova@errore.com","1828" ) );
       $this->assertEquals( 1, Login( "mario@mail.it","123" ) );
       $this->assertEquals( 1, Login( "giordano@mail.it","123" ) );
-      
+
       $this->assertEquals( 0, Signin( "mario@mail.it","123","Mario", "Rossi" ) );
       $this->assertEquals( 0, Signin( "giordano@mail.it","123","Luca", "Rossi" ) );
 
