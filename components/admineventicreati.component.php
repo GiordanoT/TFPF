c7c27420bdeefa434795d8f59bb304c41240c42d
federiico <federicocantoro99@gmail.com>
2021-05-29 10:42:54 +0200

    modifica_evento senza notifiche

--- components/admineventicreati.component.php
+++ components/admineventicreati.component.php
@@ -19,13 +19,15 @@
 			$query = "SELECT data,ora_inizio FROM data_evento,evento WHERE id_evento = '{$rowEventiCreati['e_id']}' AND admin_evento = '{$_SESSION['id']}' AND data_evento.id_evento = evento.id";
 			$resultDateEvento = getData($query);
 			$data_odierna = date("Y-m-d");
-			$ora_odierna = date("h:i");
+			date_default_timezone_set('Europe/Rome');
+			$ora_odierna = date("H:i");
 			$sem = 0;
+			
 
 			foreach($resultDateEvento as $rowDataEvento){
 				$data_limite = date('Y-m-d', strtotime('-1 day', strtotime((string)$rowDataEvento['data'])));
 				
-				if(($data_odierna < $data_limite) || ($data_odierna == $data_limite && $ora_odierna <= $rowDataEvento['ora_inizio']) )
+				if(($data_odierna < $data_limite) || ($data_odierna == $data_limite && $ora_odierna < $rowDataEvento['ora_inizio']) )
 					$sem = 1;
 			}
 
