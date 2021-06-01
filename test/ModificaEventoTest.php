55aabd4c35048e16730c3b18d223d3bc50e9e13e
federiico <federicocantoro99@gmail.com>
2021-06-01 14:42:45 +0200

    modifica evento con notifiche

--- test/ModificaEventoTest.php
+++ test/ModificaEventoTest.php
@@ -20,7 +20,7 @@ class ModificaEventoTest extends TestCase{
         
 
         $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,1,array(0,"2021-05-20","2021-09-10"),2,array(0,"2021-05-20","2021-09-11"),array(0,"18:00","16:00"),array(0,"21:00","17:00"),array(0,0,30),120));
-        $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,1,array(0,"2021-05-20","2021-09-11",""),3,array(0,"2021-05-20","2021-05-31","2021-09-14"),array(0,"18:00","16:00","10:00"),array(0,"21:00","17:00","11:00"),array(0,0,30,30),120));
+        $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,1,array(0,"2021-05-20","2021-09-11",""),3,array(0,"2021-05-20","2021-09-11","2021-09-14"),array(0,"18:00","16:00","10:00"),array(0,"21:00","17:00","11:00"),array(0,0,30,30),120));
         $this->assertEquals( 0, ModificaEvento(19,$query_evento_1,1,array(0,"2021-05-20","2021-09-11","2021-09-14"),3,array(0,"2021-05-20","2021-09-11","2021-09-14"),array(0,"18:00","16:00","10:00"),array(0,"21:00","17:00","09:00"),array(0,0,30,30),120));
         $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,1,array(0,"2021-05-20","2021-09-11"),2,array(0,"2021-05-20","2021-09-11"),array(0,"18:00","16:00"),array(0,"21:00","17:00"),array(0,0,30),120));
         $this->assertEquals( 1, ModificaEvento(19,$query_evento_1,1,array(0,"2021-05-20"),1,array(0,"2021-05-20"),array(0,"18:00"),array(0,"21:00"),array(0,0),120));
