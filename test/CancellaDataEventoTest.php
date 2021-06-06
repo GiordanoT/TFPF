fec915dd36cb4b954088ed520b31bc5e7eba5d6f
GiordanoT <giordanotinella@gmail.com>
2021-06-06 11:00:19 +0200

    fix test

--- test/CancellaDataEventoTest.php
+++ test/CancellaDataEventoTest.php
@@ -13,14 +13,17 @@ class CancellaDataEventoTest extends TestCase{
 
     public function test_example(){
 
-      //$query = 'INSERT INTO evento(id, nome, descrizione, id_categoria, tipologia, posti, admin_evento, costo, immagine, citta, concluso, approvato, sconto) VALUES (1,\'Evento Test\',\'\',1,1,100,15,0,\'\',\'Roma\',0,1,0)';
-      //$this->assertEquals( 1, setData( $query ) );
+      $query = "INSERT INTO evento(id, nome, descrizione, id_categoria, tipologia, posti, admin_evento, costo, immagine, citta, concluso, approvato, sconto) VALUES (1,'Evento Test','',1,1,100,15,0,'','Roma',0,1,0)";
+      $this->assertEquals( 1, setData( $query ) );
 
-      //$query = 'INSERT INTO data_evento(id, id_evento, data, ora_inizio, ora_fine, costo) VALUES (1,1,\'2050-01-01\',\'18:00:00\',\'20:00:00\',30)';
-      //$this->assertEquals( 1, setData( $query ) );
+      $query = "INSERT INTO data_evento(id, id_evento, data, ora_inizio, ora_fine, costo) VALUES (1,1,'2050-01-01','18:00:00','20:00:00',30)";
+      $this->assertEquals( 1, setData( $query ) );
 
-      $this->assertEquals( 1, CancellaData(208,165));
-      $this->assertEquals( 2, CancellaData(0,165));
+      $query = "INSERT INTO data_evento(id, id_evento, data, ora_inizio, ora_fine, costo) VALUES (2,1,'2050-01-01','18:00:00','20:00:00',30)";
+      $this->assertEquals( 1, setData( $query ) );
+
+      $this->assertEquals( 1, CancellaData( 1,1 ));
+      $this->assertEquals( 2, CancellaData( 0,1 ));
 
       $query = "DELETE FROM evento WHERE nome='Evento Test' ";
       $this->assertEquals( 1, setData( $query ) );
