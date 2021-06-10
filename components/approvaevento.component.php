b34ba0f455907c2f51cee0ddd242aaa577533eac
Davide <davide.palombaro@yahoo.com>
2021-06-10 12:15:05 +0200

    Bug fix

--- components/approvaevento.component.php
+++ components/approvaevento.component.php
@@ -11,14 +11,23 @@
 
     $utente = $_SESSION["id"];
 
-	$result = getData( "SELECT * FROM evento where approvato = '2'" );
+	$result = getData( "SELECT e.nome as nome, e.immagine as immagine, c.immagine as immagine_c, e.id as id  FROM evento as e join categoria as c on (c.id = e.id_categoria) where approvato = '2'" );
 	
 	foreach( $result as $row ){
             $template -> setContent( "NOME_EVENTO", $row['nome'] );
             $resultConta = getData( "SELECT count(*) as conta from data_evento where id_evento='{$row['id']}'" );
             $conta=$resultConta[0];
             $template -> setContent( "NUMERO_DATA_EVENTO", $conta['conta'] );
-			$template -> setContent( "IMMAGINE_EVENTO", $row['immagine']);				
+			if( file_exists($row['immagine']) ){
+				$template -> setContent( "IMMAGINE_EVENTO", $row['immagine'] );
+			} else {
+				if( file_exists($row['immagine_c']) ){
+					$template -> setContent( "IMMAGINE_EVENTO",$row['immagine_c']);
+				} else {
+					$template -> setContent( "IMMAGINE_EVENTO","immagini_categoria/error.png");
+				}
+			}
+							
             $template -> setContent( "ID_EVENTO", $row['id'] );
 	}
 	if( empty( $result ) || $result == 0 ){
