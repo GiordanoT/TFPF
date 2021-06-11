79131362bb5f9a4def325124deedaec22e97d675
Davide <davide.palombaro@yahoo.com>
2021-06-11 14:39:46 +0200

    PreMerge

--- test/EsistenzaDataTest.php
+++ test/EsistenzaDataTest.php
@@ -20,12 +20,12 @@ class EsistenzaData extends TestCase{
         $query = "INSERT INTO data_evento(id, id_evento, data, ora_inizio, ora_fine, costo) VALUES (1,1,'2050-01-01','18:00:00','20:00:00',30)";
         $this->assertEquals( 1, setData( $query ) );
         
-        $this->assertEquals( 1, EsistenzaDate(1) );
+        $this->assertEquals( 1, EsistenzaData(1) );
   
         $query = "INSERT INTO evento(id, nome, descrizione, id_categoria, tipologia, posti, admin_evento, costo, immagine, citta, concluso, approvato, sconto) VALUES (2,'Evento Test','',1,1,100,15,0,'','Roma',0,1,0)";
         $this->assertEquals( 1, setData( $query ) );
         
-        $this->assertEquals( 0, EsistenzaDate(2) );
+        $this->assertEquals( 0, EsistenzaData(2) );
 
         $query = "DELETE FROM evento WHERE nome='Evento Test' ";
         $this->assertEquals( 1, setData( $query ) );
