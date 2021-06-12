a025d780a549b0b1f5872547a790bdf888b1f8f6
Davide <davide.palombaro@yahoo.com>
2021-06-12 19:51:41 +0200

    Prova Test

--- include/functions/EsistenzaData.fun.php
+++ include/functions/EsistenzaData.fun.php
@@ -3,5 +3,6 @@
         $risultatoEvento = getData("SELECT count(*) as conta FROM data_evento as da WHERE da.id_evento='{$id}'");
         return $risultatoEvento[0]["conta"];
     }
+    /*Test */
 ?>
 
